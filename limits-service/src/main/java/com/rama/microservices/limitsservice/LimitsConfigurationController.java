4b8b81742537d8e9eeea9eeb8953e5ba85d63bbc
RamaDunnapotula <sairama985@gmail.com>
2021-01-03 19:46:15 +0530

    Firstcommit for limit service

---
+++ limits-service/src/main/java/com/rama/microservices/limitsservice/LimitsConfigurationController.java
@@ -0,0 +1,24 @@
+package com.rama.microservices.limitsservice;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.rama.microservices.limitsservice.bean.LimitConfiguration;
+
+@RestController
+public class LimitsConfigurationController {
+	
+	@Autowired
+	private Configuration configuration;
+	
+	@GetMapping("/limits")
+	
+		public LimitConfiguration retrieveLimitsFromConfigurations() {
+			LimitConfiguration limitConfiguration = new LimitConfiguration(configuration.getMaximum(), 
+					configuration.getMinimum());
+			return limitConfiguration;
+		}
+	}
+	
+
